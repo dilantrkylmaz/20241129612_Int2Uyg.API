f4934042ab687b9d52e5f32eed1a64bcac783a7e
Dilan Türkyılmaz <dilantr063606@gmail.com>
2026-02-22 10:38:00 +0300

    Add project files.

---
+++ Int2Uyg.API/Controllers/ProductController.cs
@@ -0,0 +1,65 @@
+﻿using Int2Uyg.API.Models;
+using Int2Uyg.API.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using Int2Uyg.API.Models;
+using Int2Uyg.API.Repositories;
+
+namespace İnt2Uyg.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductRepository _productRepository;
+        Result _result = new Result();
+        public ProductController(ProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        [HttpGet]
+        public List<Product> List()
+        {
+            var products = _productRepository.GetList();
+            return products;
+        }
+
+        [HttpGet("{id}")]
+        public Product GetById(int id)
+        {
+            var product = _productRepository.GetById(id);
+            return product;
+        }
+
+        [HttpPost]
+        public Result Add(Product model)
+        {
+            _productRepository.Add(model);
+            _result.Status = true;
+            _result.Message = "Kayıt Eklendi";
+            return _result;
+        }
+        [HttpPut]
+        public Result Update(Product model)
+        {
+            _productRepository.Update(model);
+            _result.Status = true;
+            _result.Message = "Kayıt GüncelLendi";
+            return _result;
+        }
+
+        [HttpDelete]
+        public Result Delete(int id)
+        {
+
+            _productRepository.Delete(id);
+            _result.Status = true;
+            _result.Message = "Kayıt Silindi";
+            return _result;
+
+        }
+
+    }
+}
